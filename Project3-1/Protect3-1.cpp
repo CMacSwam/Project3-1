c5ddafcafa10501374bdeeb6a3731eefada30902
Chase McCluskey <c-mccluskey.1@onu.edu>
2024-09-12 09:29:46 -0400

    FInal

--- Project3-1/Protect3-1.cpp
+++ Project3-1/Protect3-1.cpp
@@ -3,7 +3,7 @@
 * Author: Chase McCluskey
 *Date: 9/12/2024
 
-Description: Computing simple mathematic equations in C++ and using user input
+Description: Computing simple mathematic equations in C++ and using user input 
 *
 */
 
